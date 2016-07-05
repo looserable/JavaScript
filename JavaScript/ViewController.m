b3193cc79f2f9f7db8cfc59ac4deb2a613790192
lixiaotao <lixiaotao19901103@gmail.com>
2016-07-05 15:06:57 +0800

    https://github.com/looserable/JavaScript.git
    添加了一些注释的内容

--- JavaScript/ViewController.m
+++ JavaScript/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
+    
 }
 -(void)viewDidAppear:(BOOL)animated{
     self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
@@ -31,6 +31,7 @@
 }
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    //    这句话表示当前controller 等同于html 页面里的 tianbai类。那么理所当然的就可以在该controller 中就可以直接调用重写的call方法。
     self.jsContext[@"tianbai"] = self;
     self.jsContext.exceptionHandler = ^(JSContext *context, JSValue *exceptionValue) {
         context.exception = exceptionValue;
@@ -38,26 +39,26 @@
     };
 }
 
-
+//从前台往后台传值。
 - (void)call{
     NSLog(@"call");
     // 之后在回调js的方法Callback把内容传出去
     JSValue *Callback = self.jsContext[@"Callback"];
     //传值给web端
-    [Callback callWithArguments:@[@"唤起本地OC回调完成"]];
+    [Callback callWithArguments:@[@"唤起本地OC回调完成，这是前台的数据"]];
 }
 
-
+//从后台网前台传值。html 的文件里面的call方法进行了对象tianbai调用getcall的操作。
 - (void)getCall:(NSString *)callString{
     NSLog(@"Get:%@", callString);
     // 成功回调js的方法Callback
     JSValue *Callback = self.jsContext[@"alerCallback"];
     [Callback callWithArguments:nil];
     
-//    直接添加提示框
-//    NSString *str = @"alert('OC添加JS提示成功')";
-//    [self.jsContext evaluateScript:str];
-
+    //    直接添加提示框
+    //    NSString *str = @"alert('OC添加JS提示成功')";
+    //    [self.jsContext evaluateScript:str];
+    
 }
 
 
